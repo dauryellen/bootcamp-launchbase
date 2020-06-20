70dda28f9848c5549679454fa6ee6b9475191556
Daury Mendes <dauryellen@live.com>
2020-06-20 15:38:53 -0300

    iniciando módulo 2

---
+++ aulas/2-iniciando-no-front-end/aula-01.js
@@ -0,0 +1,19 @@
+/**
+ * BACK-END E FRONT-END
+ *
+ * O que é?
+ *    Back-end é o lado do servidor. É responsável pelas regras de negócio, dados, receber pedidos do cliente.
+ *    Font-end é o lado do cliente. É responsável por solicitar dados ao servidor e pela apresentação dos mesmos em tela.
+ *
+ * Cliente e Servidor
+ *    O cliente faz solicitações (requisições) de alguns serviços ao servidor
+ *    O servidor atende essas requisições respondendo o cliente com os dados solicitados
+ *    Cliente chama, servidor escuta e responde
+ *    Essa comunicação é feita através de um protocolo chamado HTTP (Hypertext Transport Protocol)
+ *
+ * HTML e CSS
+ *
+ *    HTML (Hypertext Markup Language) é responsável pela apresentação e estruturação do conteúdo (texto, imagens, vídeos)
+ *    CSS (Cascading Style Sheet) é responsável pela estilização da apresentação (cores, fontes, espaçamentos)
+ *
+ */
