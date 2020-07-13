a29bafc2dbfadff13ed3477912319e259247862d
Daury Mendes <dauryellen@live.com>
2020-07-13 15:19:18 -0300

    criando primeira rota

--- aulas/3-iniciando-no-back-end/server.js
+++ aulas/3-iniciando-no-back-end/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const server = express();
 
+server.get("/", (req, res) => {
+  return res.send("Hello");
+});
+
 server.listen(5000, () => {
   console.log("Server is running.");
 });
