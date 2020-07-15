8dc78eed9ea9ceca3d8b29fe295532d97592a6d4
Daury Mendes <dauryellen@live.com>
2020-07-15 19:15:42 -0300

    exportando e importando javascript

---
+++ aulas/3-iniciando-no-back-end/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado com JS Puro e CSS Animation",
+    duration: "57 min",
+    price: "FREE",
+  },
+  {
+    id: "2alg7MQ6_sI",
+    title: "Como Usar Git e Github na Prática",
+    duration: "33 min",
+    price: "FREE",
+  },
+  {
+    id: "GSqR2i-Pq6o",
+    title: "Desvendando a Variável This",
+    duration: "48 min",
+    price: "FREE",
+  },
+  {
+    id: "ghTrp1x_1As",
+    title: "O Que é API? REST e RESTful?",
+    duration: "33 min",
+    price: "FREE",
+  },
+  {
+    id: "rAzHvYnQ8DY",
+    title: "Array: High Order Functions",
+    duration: "54 min",
+    price: "FREE",
+  },
+  {
+    id: "c7P03kkrEG8",
+    title: "Configurando VSCode para Node.js",
+    duration: "22 min",
+    price: "FREE",
+  },
+];
