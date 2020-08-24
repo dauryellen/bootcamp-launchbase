81ad0de8299998057d31bab3b2f2a7630ccf3156
Daury Mendes <dauryellen@live.com>
2020-08-24 10:32:42 -0300

    trabalhando com formulários html

--- aulas/4-controle-de-academia/routes.js
+++ aulas/4-controle-de-academia/routes.js
@@ -2,16 +2,23 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  return res.redirect("/instructors");
+	return res.redirect("/instructors");
 });
 
 routes.get("/instructors", (req, res) => {
-  return res.render("instructors/index");
+	return res.render("instructors/index");
+});
+
+routes.get("/instructors/create", (req, res) => {
+	return res.render("instructors/create");
+});
+
+routes.post("/instructors", (req, res) => {
+	return res.send("recebido");
 });
 
 routes.get("/members", (req, res) => {
-  return res.send("members");
+	return res.send("members");
 });
-
 
 module.exports = routes;
