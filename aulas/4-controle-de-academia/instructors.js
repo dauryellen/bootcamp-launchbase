4789f3ffcadf87737f485e743835bce50fb97245
Daury Mendes <dauryellen@live.com>
2020-08-26 14:48:53 -0300

    estruturando dados dos instrutores

--- aulas/4-controle-de-academia/instructors.js
+++ aulas/4-controle-de-academia/instructors.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const data = require("./data.json");
+
 //create
 exports.post = (req, res) => {
 	const keys = Object.keys(req.body);
@@ -6,5 +9,25 @@ exports.post = (req, res) => {
 		if (req.body[key] == "") return res.send("Please, fill all the fields");
 	}
 
-	return res.send(req.body);
+	let { avatar_url, birth, name, services, gender } = req.body;
+
+	birth = Date.parse(birth);
+	const created_at = Date.now();
+	const id = Number(data.instructors.length + 1);
+
+	data.instructors.push({
+		id,
+		avatar_url,
+		name,
+		birth,
+		gender,
+		services,
+		created_at,
+	});
+
+	fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+		if (err) return res.send("Write file error!");
+
+		return res.redirect("/instructors");
+	});
 };
