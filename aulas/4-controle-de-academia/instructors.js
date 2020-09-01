1adabcff669a4720aa119c16cb4d72238ab8a3a2
Daury Mendes <dauryellen@live.com>
2020-09-01 09:14:36 -0300

    trabalhando com datas

--- aulas/4-controle-de-academia/instructors.js
+++ aulas/4-controle-de-academia/instructors.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const data = require("./data.json");
-const { age } = require("./utils");
+const { age, date } = require("./utils");
 
 //show
 exports.show = (req, res) => {
@@ -16,7 +16,9 @@ exports.show = (req, res) => {
 		...foundInstructor,
 		age: age(foundInstructor.birth),
 		services: foundInstructor.services.split(","),
-		created_at: "",
+		created_at: new Intl.DateTimeFormat("en-GB").format(
+			foundInstructor.created_at
+		),
 	};
 
 	return res.render("instructors/show", { instructor });
@@ -51,4 +53,22 @@ exports.post = (req, res) => {
 
 		return res.redirect("/instructors");
 	});
+};
+
+//update
+exports.edit = (req, res) => {
+	const { id } = req.params;
+
+	const foundInstructor = data.instructors.find((instructor) => {
+		return instructor.id == id;
+	});
+
+	if (!foundInstructor) return res.send("Instructor not found!");
+
+	const instructor = {
+		...foundInstructor,
+		birth: date(foundInstructor.birth),
+	};
+
+	return res.render("instructors/edit", { instructor });
 };
